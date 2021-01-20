b33200c98bef4c939938c71707b6451b0f4bb312
Lauren Weston <lauren.weston@codecomputerlove.com>
2021-01-20 12:46:37 +0000

    Add colcade (masonry)

--- src/js/components/NavBar.js
+++ src/js/components/NavBar.js
@@ -3,22 +3,23 @@ import { Link } from "react-router-dom";
 import robCartoon from "../../images/robCartoon.jpg";
 
 function NavBar(props) {
+
   return(
     <div className='navBar'>
       <h1 className='navBar__title'><Link to="/">ROB CARTER ART</Link></h1>
       <img className="robCartoon" alt="rob cartoon" src={robCartoon} />
       <ul className='navBar__links'>
-        <li className="navBar__link">
-          <Link to="/">Portraits</Link>
+        <li className="navBar__link--wrapper">
+          <Link to="/" className="navBar__link">Portraits</Link>
         </li>
-        <li className="navBar__link">
-          <Link to="/illustrations">Illustrations</Link>
+        <li className="navBar__link--wrapper">
+          <Link to="/illustrations" className="navBar__link">Illustrations</Link>
         </li>
-        <li className="navBar__link">
-          <Link to="/">Commissions</Link>
+        <li className="navBar__link--wrapper">
+          <Link to="/" className="navBar__link">Commissions</Link>
         </li>
-        <li className="navBar__link">
-          <Link to="/">Photo edits</Link>
+        <li className="navBar__link--wrapper">
+          <Link to="/" className="navBar__link">Photo edits</Link>
         </li>
       </ul>
     </div>
--- src/js/pages/Content.js
+++ src/js/pages/Content.js
@@ -1,89 +1,95 @@
 import React from "react";
+import Colcade from "colcade";
 
 function Content(props) {
-
   return (
     <div className='content'>
       <h3>Portraits</h3>
 
       <div className="backgroundDecoration">
-      <div className="backgroundDecoration__thin"></div>
-      <div className="backgroundDecoration__thick"></div>
+        <div className="backgroundDecoration__thin"></div>
+        <div className="backgroundDecoration__thick"></div>
       </div>
 
-      <div className="feed">
-      <div className="square">
-        
-      </div>
-      <div className="square">
-        
-      </div>
-      <div className="square">
-        
-      </div>
-      <div className="square">
-        
-      </div>
-      <div className="square">
-        
-      </div>
-      <div className="square">
-        
-      </div>
-      <div className="square">
-        
-      </div>
-      <div className="square">
-        
-      </div>
-      <div className="square">
-        
-      </div>
-      <div className="square">
-        
-      </div>
-      <div className="square">
-        
-      </div>
-      <div className="square">
-        
-      </div>
-      <div className="square">
-        
-      </div>
-      <div className="square">
-        
-      </div>
-      <div className="square">
-        
-      </div>
-      <div className="square">
-        
-      </div>
-      <div className="square">
-        
-      </div>
-      <div className="square">
-        
-      </div>
-      <div className="square">
-        
-      </div>
-      <div className="square">
-        
-      </div>
-      <div className="square">
-        
-      </div>
-      <div className="square">
-        
-      </div>
-      <div className="square">
-        
-      </div>
-      <div className="square">
-        
-      </div>
+      <div className="feed grid" data-colcade="columns: .grid-col, items: .item">
+        <div className="grid-col"></div>
+        <div className="grid-col"></div>
+        <div className="grid-col"></div>
+        <div className="grid-col"></div>
+        <div className="grid-col"></div>
+
+        <div className="item square">
+
+        </div>
+        <div className="item square">
+
+        </div>
+        <div className="item rectangle">
+
+        </div>
+        <div className="item rectangle">
+
+        </div>
+        <div className="item square">
+
+        </div>
+        <div className="item rectangle">
+
+        </div>
+        <div className="item square">
+
+        </div>
+        <div className="item rectangle">
+
+        </div>
+        <div className="item square">
+
+        </div>
+        <div className="item rectangle">
+
+        </div>
+        <div className="item square">
+
+        </div>
+        <div className="item square">
+
+        </div>
+        <div className="item rectangle">
+
+        </div>
+        <div className="item square">
+
+        </div>
+        <div className="item square">
+
+        </div>
+        <div className="item square">
+
+        </div>
+        <div className="item square">
+
+        </div>
+        <div className="item square">
+
+        </div>
+        <div className="item square">
+
+        </div>
+        <div className="item square">
+
+        </div>
+        <div className="item square">
+
+        </div>
+        <div className="item square">
+
+        </div>
+        <div className="item square">
+
+        </div>
+        <div className="item square">
+
+        </div>
       </div>
     </div>
   )
